c8e96c436dd449b49fb2d101e4abf515beff9109
Omar Eltak <153882523+OmarEltak@users.noreply.github.com>
2024-03-19 04:07:46 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let slideIndex = 0;
-showSlides();
+//showSlides();
 
 function showSlides() {
   let i;
@@ -14,3 +14,33 @@ function showSlides() {
   slides[slideIndex - 1].style.display = "block";
   setTimeout(showSlides, 3000); // Change image every 2 seconds
 }
+
+// Set the date we're counting down to
+var countDownDate = new Date("Jan 5, 2030 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function () {
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Output the result in an element with id="demo"
+  document.getElementById("secs").innerHTML = seconds;
+  document.getElementById("mins").innerHTML = minutes;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("days").innerHTML = days;
+
+  // If the count down is over, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
